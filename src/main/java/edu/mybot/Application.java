1c20ab4599f7c0f21da4d6acd88a4260cb7dc78b
Viktoriya Ryazhska <vryazhskaya@gmail.com>
2018-09-17 19:30:13 +0300

    Commit simple bot

---
+++ src/main/java/edu/mybot/Application.java
@@ -0,0 +1,18 @@
+package edu.mybot;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+
+public class Application {
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }    }
+
+}
---
+++ src/main/java/edu/mybot/Bot.java
@@ -0,0 +1,53 @@
+package edu.mybot;
+
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    /**
+     * Метод для приема сообщений.
+     * @param update Содержит сообщение от пользователя.
+     */
+    @Override
+    public void onUpdateReceived(Update update) {
+	String message = update.getMessage().getText();
+	sendMsg(update.getMessage().getChatId().toString(), message);
+    }
+
+    /**
+     * Метод для настройки сообщения и его отправки.
+     * @param chatId id чата
+     * @param s Строка, которую необходимот отправить в качестве сообщения.
+     */
+    public synchronized void sendMsg(String chatId, String s) {
+        SendMessage sendMessage = new SendMessage ();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        try {
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+        }
+    }
+
+    /**
+     * Метод возвращает имя бота, указанное при регистрации.
+     * @return имя бота
+     */
+    @Override
+    public String getBotUsername() {
+        return "VjqBot";
+    }
+
+    /**
+     * Метод возвращает token бота для связи с сервером Telegram
+     * @return token для бота
+     */
+    @Override
+    public String getBotToken() {
+        return "496494170:AAFJ1weAlaEns4TK7L19tgp9C6E_Cca7JoM";
+    }
+}
