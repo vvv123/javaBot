d313641a0fd50bbcefd8d6c2a404e054c84f972e
vvv123 <r_va@ukr.net>
2018-09-17 20:08:19 +0300

    Update Bot.java

--- src/main/java/edu/mybot/Bot.java
+++ src/main/java/edu/mybot/Bot.java
@@ -7,21 +7,12 @@ import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot {
 
-    /**
-     * Метод для приема сообщений.
-     * @param update Содержит сообщение от пользователя.
-     */
     @Override
     public void onUpdateReceived(Update update) {
 	String message = update.getMessage().getText();
 	sendMsg(update.getMessage().getChatId().toString(), message);
     }
 
-    /**
-     * Метод для настройки сообщения и его отправки.
-     * @param chatId id чата
-     * @param s Строка, которую необходимот отправить в качестве сообщения.
-     */
     public synchronized void sendMsg(String chatId, String s) {
         SendMessage sendMessage = new SendMessage ();
         sendMessage.enableMarkdown(true);
@@ -33,21 +24,13 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
-    /**
-     * Метод возвращает имя бота, указанное при регистрации.
-     * @return имя бота
-     */
     @Override
     public String getBotUsername() {
-        return "VjqBot";
+        return "MyBot";
     }
 
-    /**
-     * Метод возвращает token бота для связи с сервером Telegram
-     * @return token для бота
-     */
     @Override
     public String getBotToken() {
-        return "496494170:AAFJ1weAlaEns4TK7L19tgp9C6E_Cca7JoM";
+        return "token";
     }
 }
